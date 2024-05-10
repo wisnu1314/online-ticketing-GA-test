69504a992042e144b8126461176a3301d47930f5
Wisnu Aditya <61625865+wisnu1314@users.noreply.github.com>
2024-05-10 19:03:33 +0700

    feat:process pure HTML

---
+++ components/PreviewEvent.vue
@@ -0,0 +1,51 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+    <div>
+      <p>{{ formData.eventTitle }}</p>
+      <p>{{ formData.eventTags[0] }}</p>
+      <div v-if="isHTML" v-html="formData.eventDescription ?? null"></div>
+      <div v-if="!isHTML">{{ formData.eventDescription }}</div>
+
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PreviewEvent',
+    props: {
+      formData: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods:{
+      isHTML() {
+      // Check if eventDescription contains HTML tags
+        const containsHTML = /<[a-z][\s\S]*>/i.test(this.$props.formData.eventDescription);
+        if (containsHTML) {
+          return true
+        } else {
+          return false
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add styles for the preview page */
+  .PreviewBox{
+    width: 85%;
+    max-width: 62.5rem; 
+    margin: 0 auto;
+    overflow-y: auto;
+    background-color: white;
+    padding: 20px;
+    border:solid #f2f0e9 1px;
+    border-radius: 0.5rem;
+    margin-top:0.5rem;
+    margin-bottom: 0.5rem;
+}
+  </style>
+  
--- pages/createEvent.vue
+++ pages/createEvent.vue
@@ -1,11 +1,22 @@
 <template>
-    <EventCreateEvents/>
-  </template>
-  
-  <script>
-  import EventCreateEvents from '~/components/EventCreateEvents.vue';
-  export default {
-      components: EventCreateEvents
-  }
-  </script>
-  
+    <EventCreateEvents @back="handleBack" />
+</template>
+
+<script>
+import EventCreateEvents from '~/components/EventCreateEvents.vue';
+import PreviewEvent from '~/components/PreviewEvent.vue';
+
+export default {
+    components: {
+        EventCreateEvents,
+        // eslint-disable-next-line vue/no-unused-components
+        PreviewEvent
+    },
+    
+    methods: {
+        handleBack() {
+            this.$router.go(-1); // Go back one step in the history
+        },
+    },
+};
+</script>
